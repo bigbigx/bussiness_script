05bcbc8294e2290d131cc9c8e0d02bd127ab6006
rivest829 <956605485@qq.com>
2018-02-22 11:54:04 +0800

    数据处理：单位修正

--- build_visual_monitor.py
+++ build_visual_monitor.py
@@ -2,6 +2,8 @@
 # Author Hsinhan Chiang
 from pyecharts import Line, Page
 
+BPS_2_KBPS = 0.0009765625
+
 
 def handle_memory(mem_list):
     count = 0
@@ -36,36 +38,37 @@ def chang_value(value_list, level):
         count += 1
 
 
-def make_line(list, tips, unit, level,count):
+def make_line(list, tips, unit, level, count):
     attr_lastweek, value_lastweek = list2attr(list[0])
     attr_thisweek, value_thisweek = list2attr(list[1])
-    if count in [1,4,]:
+    if count in [1, 4, ]:
         handle_memory(value_thisweek)
         handle_memory(value_lastweek)
     chang_value(value_lastweek, level)
     chang_value(value_thisweek, level)
     line = Line(tips)
     line.add("上周", attr_lastweek, value_lastweek,
-             mark_line=["max"],is_smooth=True)
+             mark_line=["max"], is_smooth=True)
     line.add("本周", attr_thisweek, value_thisweek,
              mark_line=["max"],
-             yaxis_formatter=unit,is_smooth=True)
+             yaxis_formatter=unit, is_smooth=True)
     return line
 
 
-week_report = open('week_report-20180202', 'rb')
+week_report = open('week_report', 'rb')
 week_report_str = week_report.read().decode()
 week_report_list = week_report_str.split('FROM_UNIXTIME(clock)	value_max')
-tips_list = ["10.151.4.30 CPU", "10.151.4.30 内存", "10.151.4.30 磁盘", "10.151.4.71 CPU", "10.151.4.71 内存",
-             "10.151.4.71网络", "10.151.4.71磁盘", "核心交换机CPU", "核心交换机内存", "移动网络流量", "电信网络流量",
+tips_list = ["支付核心 CPU", "支付核心 内存", "支付核心 磁盘", "门户/认证中心 CPU", "门户/认证中心 内存",
+             "门户/认证中心网络", "门户/认证中心磁盘", "核心交换机CPU", "核心交换机内存", "移动网络流量", "电信网络流量",
              "人保金服到集团核心网RT1", "人保金服到集团核心网RT2", "人保金服到华夏银行RT1", "人保金服到华夏银行RT2"]
 unit_list = ["%", "MB", "GB", "%", "MB", "Kbps", "GB", "%", "%", "Kbps", "Kbps", "Kbps", "Kbps", "bps", "Kbps"]
-level_list = [0.01, 0.000001, 0.000000001, 0.01, 0.000001, 0.0001, 0.000000001, 1, 1, 0.0001, 0.0001,0.0001, 0.0001, 1, 0.0001]
+level_list = [0.01, 0.000001, 0.000000001, 0.01, 0.000001, BPS_2_KBPS, 0.000000001, 1, 1, BPS_2_KBPS, BPS_2_KBPS,
+              BPS_2_KBPS, BPS_2_KBPS, 1, BPS_2_KBPS]
 page = Page()
 count = 0
 list_count = 1
 for tips in tips_list:
-    line = make_line(week_report_list[list_count:list_count + 2], tips, unit_list[count], level_list[count],count)
+    line = make_line(week_report_list[list_count:list_count + 2], tips, unit_list[count], level_list[count], count)
     count += 1
     list_count += 2
     page.add(line)
